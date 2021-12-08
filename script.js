70fc498430c19128488f9db128d741dd10aa75f8
SergiuRotari <sergiu.rotari.pro@gmail.com>
2021-12-08 20:38:09 +0200

    FirstCommit

---
+++ script.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("myBurger").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myBurger").style.width = "0%";
+  }
